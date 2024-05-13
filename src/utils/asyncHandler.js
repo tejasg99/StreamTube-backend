bf1a136aa2e2d27676ecc136bf7cb7c344ef64d2
tejasg99 <tgawade092@gmail.com>
2024-05-13 19:54:44 +0530

    router and controller basic

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // Promise format
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
