6789476a00c1cff9978df950696f19f2170c1945
tejasg99 <tgawade092@gmail.com>
2024-06-08 20:26:43 +0530

    Add toggleVideoLike method in like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,8 +5,40 @@ import {ApiResponse} from "../utils/apiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
+    const {videoId} = req.params;
+    
     //TODO: toggle like on video
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid Video Id")
+    }
+
+    // if the user has already liked the video 
+    const alreadyLiked = await Like.findOne({
+        video: videoId,
+        likedBy: req.user?._id,
+    })
+
+    // unlike
+    if(alreadyLiked){
+        await Like.findByIdAndDelete(alreadyLiked._id);
+        return res 
+        .status(200)
+        .json(new ApiResponse(200, { isliked: false }, "Video unliked successfully"))
+    }
+
+    // like
+    const like = await Like.create({
+        video: videoId,
+        likedBy: req.user?._id,
+    })
+
+    if(!like){
+        throw new ApiError(500, "Unable to like a video")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, { isliked: true }, "Video liked successfully"))
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
