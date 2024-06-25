6d76df474548af9d0cf99826ef7f355377bc43c2
tejasg99 <tgawade092@gmail.com>
2024-06-25 16:52:30 +0530

    Complete toggleSubscription method

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,6 +9,36 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400, "Invalid channel id")
+    }
+
+    const existingSubscription = await Subscription.findOne({
+        subscriber: req.user?._id,
+        channel: channelId,
+    })
+
+    // Unsubscribe if exists
+    if(existingSubscription){
+        await Subscription.findByIdAndDelete(existingSubscription._id)
+        return res
+        .status(200)
+        .json(new ApiResponse(200, { isSubscribed: false }, "User unsubscribed successfully"))
+    }
+
+    const newSubscription = await Subscription.create({
+        subscriber: req.user?._id,
+        channel: channelId,
+    })
+
+    if(!newSubscription){
+        throw new ApiError(500, "Error while subscribing to the channel")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, { isSubscribed: true }, "Channel subscribed successfully"))
 })
 
 // controller to return subscriber list of a channel
