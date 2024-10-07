42b058766af6435922da0d501ba141c40aafd0a5
Tejas Gawade <tgawade092@gmail.com>
2024-10-07 16:17:47 +0530

    Fix dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -5,7 +5,8 @@ import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/apiError.js"
 import {ApiResponse} from "../utils/apiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-
+import { User } from "../models/user.model.js";
+ 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 
